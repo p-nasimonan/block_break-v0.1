441930d5c081f9d697ea8e9109829d06ba6bfb1c
p-nasi <pnasimonan@gmail.com>
2024-07-13 01:20:03 +0900

    add end_proces

--- main.py
+++ main.py
@@ -16,63 +16,83 @@ SCREEN_SIZE = config.SCREEN_SIZE
 CENTSCx, CENTSCy = config.CenterScreen 
 FPS = config.FPS
 
-def start(running): #開始関数
-    
-    # ------   インスタンス化　  -------
-    GameScreen = app.WindowObject(SCREEN_SIZE, "ブロック崩しv0.1") #ウィンドウを作成
-    world1 = app.World(stage = 1)
-    MessageText = app.GameObject(world1, ReferencePos = 'center', x = CENTSCx, y =  CENTSCy) #テキスト作成
-    Player = app.GameObject(world1, 200, 5, img_path = imgs['player'], ReferencePos = 'topleft', x = CENTSCx, y =  CENTSCy+100, stopk=0.3) #プレーヤー作成
-    Boll = app.GameObject(world1, 50, 50, img_path = imgs['boll'], ReferencePos = 'topleft', x = CENTSCx, y =  CENTSCy, vxo = config.bollvxo, vyo = config.bollvyo) #ボール作成
-    Blocks = [app.GameObject(world1, config.block_sizex, config.block_sizey, img_path = world1.block_imgs[i], ReferencePos = 'topleft', x = world1.block_xy[i][0], y =  world1.block_xy[i][1], deltable = True) for i in range(len(world1.block_xy))] #ブロック作成
+# ------   インスタンス化　  -------
+GameScreen = app.WindowObject(SCREEN_SIZE, "ブロック崩しv0.1") #ウィンドウを作成
+world1 = app.World(stage = 1)
+MessageText = app.GameObject(world1, ReferencePos = 'center', x = CENTSCx, y =  CENTSCy) #テキスト作成
+Player = app.GameObject(world1, 200, 5, img_path = imgs['player'], ReferencePos = 'topleft', x = CENTSCx, y =  CENTSCy+100, stopk=0.3) #プレーヤー作成
+Boll = app.GameObject(world1, 50, 50, img_path = imgs['boll'], ReferencePos = 'topleft', x = CENTSCx, y =  CENTSCy, vxo = config.bollvxo, vyo = config.bollvyo) #ボール作成
+Blocks = [app.GameObject(world1, config.block_sizex, config.block_sizey, img_path = world1.block_imgs[i], ReferencePos = 'topleft', x = world1.block_xy[i][0], y =  world1.block_xy[i][1], deltable = True) for i in range(len(world1.block_xy))] #ブロック作成
 
-    clock = pygame.time.Clock()
+# ---- sprite ----
+move_sprites = pygame.sprite.Group()
+move_sprites.add(Player)
+move_sprites.add(Boll)
 
-    # ---- sprite ----
-    move_sprites = pygame.sprite.Group()
-    move_sprites.add(Player)
-    move_sprites.add(Boll)
-
-    # ---- KEYCON ----
-    player_key = {
+# ---- KEYCON ----
+player_key = {
     K_RIGHT: Player.right, K_LEFT: Player.left,
     K_LSHIFT: ({Player.chv: 1}, {Player.chv: 5})
+}
 
-    }
-    # ゲームループ
-    while running:
+def display_init(clock):
+    pygame.display.update()  # 画面を更新
+    pygame.display.flip()
+    clock.tick(FPS) #フレームをFPSに従って開ける
+    GameScreen.screen.fill(color('black'))   # 画面を黒色で塗りつぶす
+
+def menu(clock):
+    while True:
         clock.tick()
-        GameScreen.screen.fill(color('black'))   # 画面を黒色で塗りつぶす
-        if not world1.isstarted:
-            MessageText.drawtext(GameScreen, text = 'スペースキーを押してスタート', x = CENTSCx, y = CENTSCy)
-            MessageText.drawtext(GameScreen, text = '操作方法',RefPos='topleft', x = CENTSCx- 600, y = CENTSCy+170, fontsize = 30)
-            MessageText.drawtext(GameScreen, text = '十字キーで移動',RefPos='topleft', x = CENTSCx-600, y = CENTSCy+220, fontsize = 20)
-            MessageText.drawtext(GameScreen, text = 'シフトキーで低速モード',RefPos='topleft', x = CENTSCx-600, y = CENTSCy+250, fontsize = 20)
-            MessageText.control({K_SPACE: world1.start})
-        else:
-            Boll.update()
-            Player.update()
+        MessageText.drawtext(GameScreen, text = 'スペースキーを押してスタート', x = CENTSCx, y = CENTSCy)
+        MessageText.drawtext(GameScreen, text = '操作方法',RefPos='topleft', x = CENTSCx- 600, y = CENTSCy+170, fontsize = 30)
+        MessageText.drawtext(GameScreen, text = '十字キーで移動',RefPos='topleft', x = CENTSCx-600, y = CENTSCy+220, fontsize = 20)
+        MessageText.drawtext(GameScreen, text = 'シフトキーで低速モード',RefPos='topleft', x = CENTSCx-600, y = CENTSCy+250, fontsize = 20)
+        MessageText.control({K_SPACE: world1.start})
 
-            # --- 描画 ---
-            Boll.draw(GameScreen)
-            Player.draw(GameScreen)
-            for block in Blocks:
-                if block.is_show:
-                    block.draw(GameScreen)
+        display_init(clock)
+        
+        if world1.isstarted:
+            return world1
 
-            # --- 操作 ---
-            Player.control(player_key)
+def stage(clock, world):
+    while True:
+        clock.tick()
+        Boll.update()
+        Player.update()
 
-            # --- 物理 ---
-            Player.physics(isstop=True)
-            Boll.physics(collision=True)
+        # --- 描画 ---
+        Boll.draw(GameScreen)
+        Player.draw(GameScreen)
+        del_blocks = []
+        for block in Blocks:
+            if block.is_show:
+                block.draw(GameScreen)
+            else:
+                del_blocks.append(block)
+                del block    
 
-        pygame.display.update()  # 画面を更新
-        pygame.display.flip()
-        clock.tick(FPS) #フレームをFPSに従って開ける
+        for block in del_blocks:
+            Blocks.remove(block)
+        
+        if len(Blocks) == 0:
+            return 0
 
-    return start(True)
 
+        # --- 操作 ---
+        Player.control(player_key)
+
+        # --- 物理 ---
+        Player.physics(isstop=True)
+        Boll.physics(collision=True)
+
+        display_init(clock)
+
+def start(running): #開始関数
+    clock = pygame.time.Clock()
+    world = menu(clock)
+    stage(clock, world)
+    running = False
 
 if __name__ == "__main__":
     start(True)
